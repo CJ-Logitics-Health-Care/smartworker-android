115c4b24ce763c32cd15c2a007bef68e7a62ca29
정승규 <jsk41755@naver.com>
2024-07-17 21:24:42 +0900

    fix: 항상 라이트모드로 볼 수 있도록 수정

--- app/src/main/java/com/devjsg/cj_logistics_future_technology/presentation/theme/Theme.kt
+++ app/src/main/java/com/devjsg/cj_logistics_future_technology/presentation/theme/Theme.kt
@@ -2,10 +2,8 @@ package com.devjsg.cj_logistics_future_technology.presentation.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -14,12 +12,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
@@ -39,19 +31,13 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun CJLogisticsFutureTechnologyTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val context = LocalContext.current
+        dynamicLightColorScheme(context)
+    } else {
+        LightColorScheme
     }
 
     val context = LocalContext.current
