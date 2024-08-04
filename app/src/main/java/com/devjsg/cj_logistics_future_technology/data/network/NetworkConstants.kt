cae4c636188e4eeb76704a8bd28eb12b1e910543
정승규 <jsk41755@naver.com>
2024-08-05 01:06:26 +0900

    fix: 도메인 주소 변경

--- app/src/main/java/com/devjsg/cj_logistics_future_technology/data/network/NetworkConstants.kt
+++ app/src/main/java/com/devjsg/cj_logistics_future_technology/data/network/NetworkConstants.kt
@@ -1,5 +1,5 @@
 package com.devjsg.cj_logistics_future_technology.data.network
 
 object NetworkConstants {
-    const val BASE_URL = "http://ec2-3-35-138-197.ap-northeast-2.compute.amazonaws.com:8080/api/v1/"
+    const val BASE_URL = "https://cj-api.serial-blog.com/api/v1/"
 }
